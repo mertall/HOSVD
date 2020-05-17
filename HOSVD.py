bb906932f6f1f91986e8dc7a38491cbaf84ebd3f
mridulsar <mridul1399@gmail.com>
2020-05-17 00:28:04 -0700

    Update HOSVD.py

--- HOSVD.py
+++ HOSVD.py
@@ -5,8 +5,9 @@ def HOSVD(A):
     #S is n(1) x ... x n(d) tensor : A x1 U(1) x2 U(2) ... xd U(d)
     S = A
 
-    for k in 1:length(A.size()):
-        C = A[k,:].numpy() ##may need to make sure columns are arranged in increasing order whenever this line is run
+    for k in range(A.size()):
+        C = A[k,:].numpy() ##may need to make sure columns are arranged in increasing order
         [U[k],Sig,V] = svd(C)
         t_U(k) = numpyu.transpose(U(k))
-        S = numpy.tesnrodot(S,U(k),k)
+        S = numpy.tensordot(S,t_U(k),k)
+    return S, U
