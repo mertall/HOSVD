04f25368428cd1a7f406c8c707e2f2e8bd96ee41
mertall <42987995+mertall@users.noreply.github.com>
2020-07-20 08:39:22 -0700

    Update HOSVD.py

--- HOSVD.py
+++ HOSVD.py
@@ -11,3 +11,16 @@ def HOSVD(A):
         t_U(k) = numpyu.transpose(U(k))
         S = numpy.tensordot(S,t_U(k),k)
     return S, U
+
+def SHOSVD(A):
+    #A is n(1) x ... x n(d) tensor
+    #U is d cell array with U(k) being the left singular vector of a's mode-k unfolding
+    #S is n(1) x ... x n(d) tensor : A x1 U(1) x2 U(2) ... xd U(d)
+    S = A
+
+    for k in range(A.size()):
+        C = A[k,:].numpy() ##may need to make sure columns are arranged in increasing order
+        [U[k],Sig,V] = svds(C)
+        t_U(k) = numpyu.transpose(U(k))
+        S = numpy.tensordot(S,t_U(k),k)
+    return S, U
