8aa9fb53606d35db0d5ff5f5c67c0609a2848a80
mridulsar <mridul1399@gmail.com>
2020-11-29 14:28:55 -0800

    updated alg

--- HOSVD.py
+++ HOSVD.py
@@ -1,31 +1,29 @@
 #Cornell Summer School Charles F. Van Loan
-class HOSVD(object):
-    def dense(A):
-        #A is n(1) x ... x n(d) tensor
-        #U is d cell array with U(k) being the left singular vector of a's mode-k unfolding
-        #S is n(1) x ... x n(d) tensor : A x1 U(1) x2 U(2) ... xd U(d)
-        S = A
+import numpy
+from numpy import svd, svds
 
-        for k in range(A.size()):
-            C = A[k,:].numpy() ##may need to make sure columns are arranged in increasing order
-            [U[k],Sig,V] = svd(C)
-            t_U = numpy.transpose(U)
-            S = numpy.tensordot(S,t_U[k],k)
-        return S, U, V
+def dense(A):
+    #A is n(1) x ... x n(d) tensor
+    #U is d cell array with U(k) being the left singular vector of a's mode-k unfolding
+    #S is n(1) x ... x n(d) tensor : A x1 U(1) x2 U(2) ... xd U(d)
+    S = A
 
-    def sparse(A):
-        #A is n(1) x ... x n(d) tensor
-        #U is d cell array with U(k) being the left singular vector of a's mode-k unfolding
-        #S is n(1) x ... x n(d) tensor : A x1 U(1) x2 U(2) ... xd U(d)
-        S = A
+    for k,_ in range(A.size()):
+        C = A[k,:].numpy() ##may need to make sure columns are arranged in increasing order
+        [U,_,_] = svd(C)
+        t_U = numpy.transpose(U)
+        S(k,:).append(numpy.tensordot(S,t_U,k))
+    return S, t_U
 
-        for k in range(A.size()):
-            C = A[k,:].numpy() ##may need to make sure columns are arranged in increasing order
-            [U[k],Sig,V] = svds(C)
-            t_U = numpy.transpose(U)
-            S = numpy.tensordot(S,t_U[k],k)
-        return S, U, V
-    def emotions(S,t_U,V)
-        eigen = t_U * S * V
-        unique_info = numpy.transpose(t_U)
-    # emotions is S? Clasification will use LS/QReither solving for min||C_e*a_e-z||_2 (LS) or R_e*a_e=QT_ez(QE)
+def sparse(A):
+    #A is n(1) x ... x n(d) tensor
+    #U is d cell array with U(k) being the left singular vector of a's mode-k unfolding
+    #S is n(1) x ... x n(d) tensor : A x1 U(1) x2 U(2) ... xd U(d)
+    S = A
+
+    for k,_ in range(A.size()):
+        C = A[k,:].numpy() ##may need to make sure columns are arranged in increasing order
+        [U,_,_] = svds(C)
+        t_U = numpy.transpose(U)
+        S(k,:).append(numpy.tensordot(S,t_U,k))
+    return S, t_U
